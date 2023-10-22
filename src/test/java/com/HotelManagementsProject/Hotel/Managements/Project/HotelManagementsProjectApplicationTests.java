898864f4e82f83946ec4057da22be36ae577a054
Duaa <Duaahil44@gmail.com>
2023-10-22 13:48:24 +0300

    first commit

---
+++ src/test/java/com/HotelManagementsProject/Hotel/Managements/Project/HotelManagementsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HotelManagementsProject.Hotel.Managements.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelManagementsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
